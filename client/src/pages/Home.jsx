9e1dc44980efae9f3e6b71e609de7b3c4536db41
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-02-08 16:39:32 -0500

    Created Home Page UI

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import "../App.css";
+import { useRef } from "react";
+
+const Home = () => {
+  const fileInputRef = useRef();
+
+  return (
+    <div className="container">
+      <h1>FileBin</h1>
+      <p>Convenient file sharing in three steps without registration</p>
+
+      <p>
+        <span>1</span>
+        <input type="file" ref={fileInputRef} style={{ display: "none" }} />
+        <button onClick={() => fileInputRef.current.click()}>Select Files to Upload</button>
+        or drag-and-drop files into this browser window.{" "}
+      </p>
+      <p>
+        <span>2</span> Wait until they complete.
+      </p>
+      <p>
+        <span>3</span> The files will be available at URL which is a link you can share.
+      </p>
+
+      <p className="info">The file uploads will cancel if you move away from this page before they complete. Uploaded files can be deleted manually at any time and will in any case be deleted automatically 6 days from now.</p>
+    </div>
+  );
+};
+
+export default Home;
