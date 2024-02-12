2c5c0a7294a00f9ac54c969ba2242fb2b00deef4
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-02-11 19:26:25 -0500

    dynamically display the new randomly generated URL on the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import "../App.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getSignedUrl } from "../services/api";
 
 const Home = () => {
+  const [url, setUrl] = useState("");
+  const [file, setFile] = useState("");
+
   const fileInputRef = useRef();
 
   useEffect(() => {
     const getData = async () => {
       const response = await getSignedUrl();
+      setUrl(response.url);
     };
     getData();
   }, []);
@@ -19,7 +23,7 @@ const Home = () => {
 
       <p>
         <span>1</span>
-        <input type="file" ref={fileInputRef} style={{ display: "none" }} />
+        <input type="file" ref={fileInputRef} style={{ display: "none" }} onChange={() => setFile(e.target.files)} />
         <button onClick={() => fileInputRef.current.click()}>Select Files to Upload</button>
         or drag-and-drop files into this browser window.{" "}
       </p>
@@ -27,7 +31,7 @@ const Home = () => {
         <span>2</span> Wait until they complete.
       </p>
       <p>
-        <span>3</span> The files will be available at URL which is a link you can share.
+        <span>3</span> The files will be available at <a href={url.split("?")[0]}>{url.split("?")[0]}</a> which is a link you can share.
       </p>
 
       <p className="info">The file uploads will cancel if you move away from this page before they complete. Uploaded files can be deleted manually at any time and will in any case be deleted automatically 6 days from now.</p>
